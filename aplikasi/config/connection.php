72ffa6a7c3168ebcd06ebd20b85a7f53656c0a9f
fairuzmk <fairuzdeveloper@gmail.com>
2025-01-27 15:53:54 +0700

    cobaterus

--- aplikasi/config/connection.php
+++ aplikasi/config/connection.php
@@ -1,5 +1,10 @@
 <?php
 
-$koneksi = mysqli_connect('localhost','root','','db_aplikasi');
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$db_name = "db_aplikasi";
+
+$koneksi = mysqli_connect($host,$dbusername,$dbpassword,$db_name);
 
 ?>
