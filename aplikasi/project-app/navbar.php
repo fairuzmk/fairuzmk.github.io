00569abad05547711e13b139f5b90900d21cd7db
Fairuz_lenovo <fairuzdeveloper@gmail.com>
2025-03-14 07:44:04 +0700

    nav-profile

--- aplikasi/project-app/navbar.php
+++ aplikasi/project-app/navbar.php
@@ -35,18 +35,37 @@
         </div>
       </li>
 
+      <!-- Messages Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <div class="nav-profile" data-toggle="dropdown" href="#">
+            <div class="image">
+                <img src="dist/img/avatar4.png" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="sapaan">
+                <a href="#" class="d-block"><strong>Hai</strong>, <?php echo strtok($_SESSION['nama'], ' '); ?></a>
+            </div>
+        </div>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <span class="dropdown-item dropdown-header">15 Notifications</span>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-envelope mr-2"></i> 4 new messages
+            <span class="float-right text-muted text-sm">3 mins</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-users mr-2"></i> 8 friend requests
+            <span class="float-right text-muted text-sm">12 hours</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="../config/logout.php" class="dropdown-item dropdown-footer text-danger">
+            <i class="fas fa-sign-out-alt nav-icon mr-2"></i> Sign Out     
+          </a>
+          
+        </div>
+      </li>
       
       
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
     </ul>
   </nav>
   <!-- /.navbar -->
