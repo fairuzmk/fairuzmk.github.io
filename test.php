289463772eb054de619f823c88489bc21529a2f7
fairuzmk <fairuzdeveloper@gmail.com>
2025-04-23 21:16:44 +0700

    data2

--- test.php
+++ test.php
@@ -2,26 +2,26 @@
     //Hanya Mengambil data h-index atau nama saja
     $apiKey = "d3a7416a53291eb0bb1a85770755681a";
     $authorId = "57931825700"; // Ganti dengan Scopus Author ID
-    // $url = "https://api.elsevier.com/content/author/author_id/57931825700?view=metrics";
+    $url2 = "https://api.elsevier.com/content/author/author_id/57931825700?view=metrics";
     // $url2 = "https://api.elsevier.com/content/author?author_id=$authorId";
     
-    // $options = [
-    //     "http" => [
-    //         "header" => "X-ELS-APIKey: $apiKey\r\nAccept: application/json\r\n",
-    //         "method" => "GET",
-    //     ],
-    // ];
+    $options = [
+         "http" => [
+             "header" => "X-ELS-APIKey: $apiKey\r\nAccept: application/json\r\n",
+             "method" => "GET",
+         ],
+     ];
     
     // $context = stream_context_create($options);
     // $response = file_get_contents($url, false, $context);
     // $data = json_decode($response, true);
-    // $context2 = stream_context_create($options);
-    // $response = file_get_contents($url2, false, $context2);
-    // $data2 = json_decode($response, true);
+    $context2 = stream_context_create($options);
+    $response2 = file_get_contents($url2, false, $context2);
+    $data2 = json_decode($response2, true);
 
     // var_dump($data2);
     // echo "Nama Penulis: " . $data['author-retrieval-response'][0]['author-profile']['preferred-name']['surname'] . "\n";
-    echo "H-Index: " . $data['author-retrieval-response'][0]['h-index'];
+    echo "H-Index: " . $data2['author-retrieval-response'][0]['h-index'];
 
     
     $url = "https://api.elsevier.com/content/search/scopus?query=AU-ID($authorId)&apiKey=$apiKey";
