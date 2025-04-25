099f74e85de7f73b069be4876c50d02bba43b1d3
fairuzmk <fairuzdeveloper@gmail.com>
2025-04-25 17:49:41 +0700

    remove css bs 5.3

--- aplikasi/index.php
+++ aplikasi/index.php
@@ -170,7 +170,7 @@
 <script src="loginanimation.js"></script>
 <!-- Toastr -->
 <script src="project-app/plugins/toastr/toastr.min.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 
